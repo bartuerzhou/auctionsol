2cf0e7215f07dd4110665e130e175b36b7c45331
Bartuer Zhou <bazhou@microsoft.com>
2023-01-01 19:16:17 +0800

    web3.js SDK sample

--- sdk/env.sh
+++ sdk/env.sh
@@ -1,5 +1,5 @@
 export LOCAL_EVM_PROVIDER=http://localhost:8545
 export WS_EVM_PROVIDER=ws://localhost:8545
 export LOCAL_EVM_CONTRACT_ABI=/mnt/c/Users/bazhou/local/src/sc/src/sdk/Auction.abi
-export LOCAL_EVM_CONTRACT_ADDR=0xaCea1B8aA95195cFcbe5C55b5DB2061C94961a4F
+export LOCAL_EVM_CONTRACT_ADDR=0x82113ec6521E70D928ec1ed784057F98b094AA1C
 export LOCAL_EVM_WALLET_MNEMONIC="street warm desk ugly muffin sort zebra motion mad cream follow midnight"
--- sdk/web3.js
+++ sdk/web3.js
@@ -17,18 +17,27 @@ async function run() {
     const ABI = await get_abi(process.env.LOCAL_EVM_CONTRACT_ABI);
     const Contract = new web3.eth.Contract(ABI, address);
     const accounts = await web3.eth.getAccounts();
+    // bid API
     const bid_res = await Contract.methods.bidAuction(1).send({
-        value: 1015,
+        value: 1018,
         from: accounts[1]
     });
-    console.log(`to: ${bid_res.to}`);
-    console.log(`gas: ${bid_res.gasUsed}`);
-    console.log(`Received event: ${bid_res.events.Received.returnValues[0]} ${bid_res.events.Received.returnValues[1]}`);
-    // await Contract.methods.removeTimeout();
-    // await Contract.methods.bidAuction(1).send({
-    //     value: 1015,
-    //     from: accounts[1]
-    // });
+    console.log(`bid to: ${bid_res.to}`);
+    console.log(`bid gas: ${bid_res.gasUsed}`);
+    console.log(`bid Received event: ${bid_res.events.Received.returnValues[0]} ${bid_res.events.Received.returnValues[1]}`);
+    await Contract.methods.removeTimeout(3).send({
+        from: accounts[3]
+    });
+    const timeout = await Contract.methods.checkTimeout(3).call();
+    console.log(`timeout: ${timeout}`);
+    // reclaim API
+    const reclaim_res = await Contract.methods.doneAuction(3).send({
+        from: accounts[0]
+    });
+    const reclaim = await Contract.methods.queryNFTOwner(3, accounts[0]).call();
+    console.log(`reclaim: ${reclaim}`);
+    console.log(`reclaim to: ${reclaim_res.to}`);
+    console.log(`reclaim gas: ${reclaim_res.gasUsed}`);
 }
 
 run();
